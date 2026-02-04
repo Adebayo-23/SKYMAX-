b27df7bf037818d623770eda98c779589fa25883
Adebayo-23 <adebayoabdulrahman173@gmail.com>
2026-02-04 14:20:04 +0100

    Update server.ts

--- netlify/functions/server.ts
+++ netlify/functions/server.ts
@@ -1,7 +1,16 @@
 import { createRequestHandler } from "@remix-run/netlify";
 import * as build from "../../build/server/index.js";
 
+// The build export may be typed with a different @remix-run package instance
+// in the local node_modules tree which can cause TypeScript to complain when
+// mixing types from different copies of @remix-run packages. Cast to `any`
+// here to avoid duplicate-type incompatibilities in the Netlify function.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const typedBuild = build as unknown as any;
+
 export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV || "production",
+  build: typedBuild,
+  getLoadContext() {
+    return {};
+  },
 });
