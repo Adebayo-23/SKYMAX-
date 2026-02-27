416e5a65d7e74bc042c2f432e03522008383ef3d
Adebayo-23 <adebayoabdulrahman173@gmail.com>
2026-02-27 16:19:14 +0100

    Update input.tsx

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input className="border rounded px-2 py-1 w-full" {...props} />;
+  const { value, onChange, className, ...rest } = props;
+
+  // If a `value` is provided but no `onChange`, treat the input as readOnly
+  // to avoid React throwing a production error for controlled inputs.
+  const readOnly = value !== undefined && onChange === undefined ? true : props.readOnly;
+
+  return <input className={(className || '') + ' border rounded px-2 py-1 w-full'} value={value} onChange={onChange} readOnly={readOnly} {...rest} />;
 }
